8dd8ce7415453dbdaca3b6628051552312703655
Moosky-1 <m.navti@gmail.com>
2023-06-26 12:30:20 +0100

    Made a copy of the vpc module

---
+++ 03-tier-infra-modules-copy/backend.tf
@@ -0,0 +1,10 @@
+#store the terraform state file in s3
+
+terraform {
+  backend "s3" {
+    bucket  = "april28081993demo"
+    key     = "three-tier/backend/moosky-website-ecs.tfstate"
+    region  = "eu-west-1"
+    profile = "default"
+  }
+}
